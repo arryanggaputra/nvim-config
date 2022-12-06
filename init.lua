5a105a69095725e6b967876d43c96a97c2c4ac4d
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2022-12-06 09:25:16 +0700

    :sparkles: add lualine

--- init.lua
+++ init.lua
@@ -1,10 +1,30 @@
 vim.opt.number = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.expandtab = true
 vim.opt.cursorline = true
 vim.opt.laststatus = 2
-
-
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = "fish"
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" })
 
 require("core.plugins")
 require("core.keymaps")
@@ -13,15 +33,10 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
-
 require("core.nvim-tree")
 require("core.treesitter")
 require("core.toggleterm")
 require("core.indentline")
-require('lualine').setup()
-require('nvim-web-devicons').setup()
-require("mason").setup()
-
-
-
-vim.cmd.colorscheme "catppuccin"
+require("lualine").setup()
+require("nvim-web-devicons").setup()
+vim.cmd.colorscheme("catppuccin")
